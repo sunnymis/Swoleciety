36eef57a2ad26575b3791b0d98ceda8539537c71
Sunny Mistry <sunnymis@buffalo.edu>
2016-02-27 21:05:50 -0500

    started drag functionality

--- app/js/controllers/Controllers.js
+++ app/js/controllers/Controllers.js
@@ -10,12 +10,48 @@ swolecietyControllers.controller('MainController', ['$scope', 'Exercise',
     function ($scope, Exercise) {
         $scope.exercises = Exercise.query();
     }
-]);
+    ]);
 
 
-swolecietyControllers.controller('CalendarController', ['$scope', 
-   function($scope) {
-       $scope.days = ['Sunday', 'Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+swolecietyControllers.controller('CalendarController', ['$scope',
+    function ($scope) {
+        $scope.days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+        
+        
+
+        $scope.handleDragStart = function(ev) {
+            this.style.opacity = '0.4';
+            ev.dataTransfer.setData("text", ev.target.id);                                           
+        };
+        $scope.handleDragOver = function(ev) {
+            ev.preventDefault();
+            ev.dataTransfer.dropEffect = 'move';
+        };
+        $scope.handleDragEnter = function(ev) {
+            alert('over');
+            this.classList.add('over');
+        };
+        $scope.handleDragLeave = function(ev) {
+            this.classList.remove('over');
+        };
+        
+        $scope.exercise = document.getElementById('exercise1');
+        $scope.weekday = document.getElementById('weekday');
+        
+        $scope.exercise.addEventListener('dragstart', $scope.handleDragStart, false);
+        
+        var days = document.querySelectorAll('#weekday');
+        [].forEach.call(days, function(day) {
+            day.addEventListener('dragover', $scope.handleDragOver, false);
+            day.addEventListener('dragenter', $scope.handleDragEnter, false);
+            day.addEventListener('dragleave', $scope.handleDragLeave, false);
+        });
+        
    }
 ]);
 
+/*
+        $scope.weekday.addEventListener('dragover', $scope.handleDragOver, false);
+        $scope.weekday.addEventListener('dragenter', $scope.handleDragEnter, false);
+        $scope.weekday.addEventListener('dragleave', $scope.handleDragLeave, false);
+        */
