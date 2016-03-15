3646a7580fdee171656c5b5b0711a1a5baaab414
Sunny Mistry <sunnymis@buffalo.edu>
2016-03-15 11:20:52 -0400

    changed config to inject annotation, added iife

--- app/js/app.js
+++ app/js/app.js
@@ -1,27 +1,36 @@
-'use strict';
+(function() {
+    'use strict';
 
-// Declare app level module which depends on views, and components
-angular.module('SwolecietyApp', [
-    'ngRoute',
-    'SwolecietyControllers',
-    'SwolecietyServices',
-    'firebase'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/exercises', {
-        templateUrl: 'partials/exercises-list.html',
-        controller: 'MainController'
-        })
-        .when('/calendar', {
-        templateUrl: 'partials/calendar.html',
-        controller: 'CalendarController'
-       })
-        .when('/login', {
-        templateUrl: 'partials/login.html',
-        controller: 'LoginController'
-        })    
-      .otherwise( {
-        redirectTo: '/exercises'
-      });
-}]);
+    // Declare app level module which depends on views, and components
+    angular.module('SwolecietyApp', [
+        'ngRoute',
+        'SwolecietyControllers',
+        'SwolecietyServices',
+        'firebase'
+    ]).
+    config(configFunction); 
+
+    configFunction.$inject = ['$routeProvider'];
+
+    function configFunction($routeProvider) {
+        $routeProvider.when('/exercises', {
+            templateUrl: 'partials/exercises-list.html',
+            controller: 'MainController'
+            })
+            .when('/calendar', {
+            templateUrl: 'partials/calendar.html',
+            controller: 'CalendarController'
+           })
+            .when('/login', {
+            templateUrl: 'partials/login.html',
+            controller: 'LoginController'
+            })    
+          .otherwise( {
+            redirectTo: '/exercises'
+          });
+    }
+})();
+
+    
+  
 
