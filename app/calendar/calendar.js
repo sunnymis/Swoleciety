da1b3b2ceb3cacc9280353b6c9686ba0bda73358
Sunny Mistry <sunnymis@buffalo.edu>
2016-03-24 22:55:27 -0400

    pushing of workouts statically is working

--- app/calendar/calendar.js
+++ app/calendar/calendar.js
@@ -5,21 +5,27 @@
         .module('swoleciety.calendar')
         .controller('CalendarController',CalendarController);
     
-    CalendarController.$inject = ['$firebaseArray', 'FIREBASE_URL'];
+    CalendarController.$inject = ['$firebaseArray', '$firebaseObject', 'FIREBASE_URL'];
     
-    function CalendarController($firebaseArray, FIREBASE_URL) {
+    function CalendarController($firebaseArray, $firebaseObject, FIREBASE_URL) {
         var vm = this; 
         vm.days = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'];
         vm.weeklyExercises = {};
         angular.forEach(vm.days, function(day) {
             vm.weeklyExercises[day] = [];
         });
-        vm.a;
-        
+            
         vm.loadExercisesForDay = function() {
             var ref = new Firebase(FIREBASE_URL).child('users').child('smistry').child('exercises').child('monday');
             var allExercises = $firebaseArray(ref);
-            console.log(allExercises);
+            allExercises.$loaded()
+            .then(function() { 
+                angular.forEach(allExercises, function(value) {
+                    vm.weeklyExercises['monday'].push(value.$id); 
+                });
+               
+            });
+            
             
 //            allExercises.on("child_added", function(snapshot) {
 //                vm.weeklyExercises[snapshot.key()] = snapshot.val();
