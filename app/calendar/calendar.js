2a7a880613e5e3f7f016d49f6f1d4254337b0457
Sunny Mistry <sunnymis@buffalo.edu>
2016-03-25 20:49:58 -0400

    adding workouts for each day

--- app/calendar/calendar.js
+++ app/calendar/calendar.js
@@ -16,37 +16,16 @@
         });
             
         vm.loadExercisesForDay = function() {
-            var ref = new Firebase(FIREBASE_URL).child('users').child('smistry').child('exercises').child('monday');
-            var allExercises = $firebaseArray(ref);
-            allExercises.$loaded()
-            .then(function() { 
-                angular.forEach(allExercises, function(value) {
-                    vm.weeklyExercises['monday'].push(value.$id); 
-                });
-               
+            angular.forEach(vm.days, function(day) {
+                var ref = new Firebase(FIREBASE_URL + '/users/smistry/exercises/').child(day);
+                var allExercises = $firebaseArray(ref);
+                allExercises.$loaded()
+                .then(function() { 
+                    angular.forEach(allExercises, function(value) {
+                        vm.weeklyExercises[day].push(value.$id); 
+                    });
+                }); 
             });
-            
-            
-//            allExercises.on("child_added", function(snapshot) {
-//                vm.weeklyExercises[snapshot.key()] = snapshot.val();
-//                console.log(vm.weeklyExercises);
-//            });
-            
-//                var allExercises = $firebaseArray(ref);
-//                ref.once("value", function(snapshot) {  
-//                    var allExercises = $firebaseArray(ref);
-//                    angular.forEach(allExercises, function(e) {
-////                        vm.weeklyExercises[day].push(e);
-//                    });     
-//                })
-//                allExercises.$loaded()
-//                .then(function() {
-//                    angular.forEach(allExercises, function(e) {
-//                        vm.weeklyExercises[day].push(e);
-//                    });       
-//                });
-                        
-               
         }
     }
     
