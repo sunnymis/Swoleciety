39e7f327d2f71b387ff6c698efc763a589562c1c
Sunny Mistry <sunnymis@buffalo.edu>
2017-02-06 23:46:05 -0500

    Integrated Jest and it works

---
+++ app/component/component.test.js
@@ -0,0 +1,15 @@
+
+import React from 'react';
+import {shallow} from 'enzyme';
+import CheckboxWithLabel from './component';
+import ReactTestUtils from 'react-addons-test-utils';
+
+it('CheckboxWithLabel changes the text after click', () => {
+  expect(1).toBe(1);
+  // const checkbox = ReactTestUtils.renderIntoDocument(
+  //   <CheckboxWithLabel labelOn="On" labelOff="Off" />
+  // );
+  // const checkboxNode = ReactDOM.findDOMNode(checkbox);
+  // expect(checkboxNode.textContent).toEqual('Off');
+  // 
+});
