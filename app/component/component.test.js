3a1ee11cc34ce3d122a8ffa4254449d11ea8d1f2
Sunny Mistry <sunnymis@buffalo.edu>
2017-02-08 22:57:35 -0500

    Mocha, Chai, Enzyme testing work

--- app/component/component.test.js
+++ app/component/component.test.js
@@ -3,16 +3,16 @@ import React from 'react';
 import { mount, shallow } from 'enzyme';
 import {expect} from 'chai';
 
-import Avatar from './Avatar';
+import App from './App';
 
-describe('<Avatar/>', function () {
+describe('<App/>', function () {
   it('should have an image to display the gravatar', function () {
-    const wrapper = shallow(<Avatar/>);
+    const wrapper = shallow(<App/>);
     expect(wrapper.find('img')).to.have.length(1);
   });
 
   it('should have props for email and src', function () {
-    const wrapper = shallow(<Avatar/>);
+    const wrapper = shallow(<App/>);
     expect(wrapper.props().email).to.be.defined;
     expect(wrapper.props().src).to.be.defined;
   });
@@ -21,10 +21,10 @@ describe('<Avatar/>', function () {
 describe('Array', () => {
   describe('#indexOf()', () => {
     it('should return -1 when the value is not present', () => {
-      assert.equal(-1,[1,2,3].indexOf(4));
+      expect([1,2,3].indexOf(4)).to.equal(-1);
     });
     it('should return 0 when the value is 1', () => {
-      assert.equal(0,[1,2,3].indexOf(1));
+      expect([1,2,3].indexOf(1)).to.equal(0);
     });
   });
 });
