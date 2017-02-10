64960cd31b869591575fe940913d5d5d45508352
Sunny Mistry <sunnymis@buffalo.edu>
2017-02-10 00:19:32 -0500

    Added coveralls. Hopefully it works

--- karma.conf.js
+++ karma.conf.js
@@ -83,7 +83,12 @@ module.exports = function(config) {
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
     reporters: ['spec', 'coverage'],
     coverageReporter: {
-        type: 'text'
+
+        dir: 'coverage',
+        reporters: [
+            { type: 'html', subdir: 'report-html' },
+            { type: 'lcov', subdir: 'report-lcov' }
+        ]
     },
     
   })
