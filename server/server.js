72cb29194d31c65a03f577395f7112495e2d90d4
Sunny Mistry <sunnymis@buffalo.edu>
2017-02-24 21:38:34 -0500

    Moved port to a variable

--- server/server.js
+++ server/server.js
@@ -2,8 +2,9 @@ const express = require('express');
 const exercisesRoute = require('./routes/exercises');
 
 const app = express();
+const port = 3000; 
 
 app.use('/exercises', exercisesRoute);
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
